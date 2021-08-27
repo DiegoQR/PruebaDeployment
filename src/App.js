be77ac64135d45fa90fe905bf700a713236f7034
Diego Quinones <diegoqr2000@gmail.com>
2021-08-26 22:47:10 -0400

    El bot saluda segun edad o genero

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
-alert("Presiona OK si estas list@ para empezar con javascript")
+const nombre = document.querySelector("#nombre-input");
+const edad = document.querySelector("#edad-input");
+const genero = document.querySelector("#genero-input");
+const form = document.querySelector("#saludador-form");
+
+function greetingGenderSelector(name, gender){
+    if(gender == "Masculino" ){
+        var gend = "señor";
+    } else {
+        var gend = "señora";
+    }
+    return gend;
+}
+
+function greetingSelector(name, age, gender){
+    if(age < 25 ){
+        var message = "Buenos dias" + " " + greetingGenderSelector(name,gender) + " " + name;
+    } else {
+        var message = "Le deseo buenos dias," + " " + greetingGenderSelector(name,gender) + " " + nombre.value;
+    }
+    return message;
+}
+
+form.addEventListener("submit", event => {
+    alert(greetingSelector(nombre.value, edad.value, genero.value));
+})
